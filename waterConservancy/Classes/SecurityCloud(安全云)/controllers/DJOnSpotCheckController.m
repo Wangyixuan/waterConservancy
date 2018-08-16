870c3bb0252a61d4a314bcfd4c763bd27dab62c6
liudejian <liudejian@syberos.com>
2018-08-16 14:34:37 +0800

    1

---
+++ waterConservancy/Classes/SecurityCloud(安全云)/controllers/DJOnSpotCheckController.m
@@ -0,0 +1,37 @@
+//
+//  DJOnSpotCheckController.m
+//  waterConservancy
+//
+//  Created by liu on 2018/8/16.
+//  Copyright © 2018年 com.yx.waterConservancy. All rights reserved.
+//
+
+#import "DJOnSpotCheckController.h"
+
+@interface DJOnSpotCheckController ()
+
+@end
+
+@implementation DJOnSpotCheckController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
